31879c6bfd81cb4bb1066f336bed9a409e157bb3
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-02 00:20:42 +0300

    add segue to vc from tablewview

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by ismail harmanda on 2.11.2023.
+//
+
+import UIKit
+import CoreData
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
